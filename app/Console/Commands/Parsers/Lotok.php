a8102875a78907ac5e7d2eee0dfaf3cc2a2c6771
Rostik <rostislav.kh@zina.com.ua>
2023-03-12 10:26:02 +0200

    added command

---
+++ app/Console/Commands/Parsers/Lotok.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Parsers;
+
+use Illuminate\Console\Command;
+
+class Lotok extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'parser:lotok';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Parse - https://lotok.ua/discount/cat1';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
