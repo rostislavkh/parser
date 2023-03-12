91a8324044654fc440d8d1924ae1c4be4724be4b
Rostik <rostislav.kh@zina.com.ua>
2023-03-12 14:02:08 +0200

    added try catch

--- app/Console/Commands/Parsers/Lotok.php
+++ app/Console/Commands/Parsers/Lotok.php
@@ -35,45 +35,49 @@ class Lotok extends Command
      */
     public function handle(): void
     {
-        $document = new Document('https://lotok.ua/discount/cat1', true);
+        try {
+            $document = new Document('https://lotok.ua/discount/cat1', true);
 
-        $main_slider_products = $document->find('.products-slider__slide');
+            $main_slider_products = $document->find('.products-slider__slide');
 
-        $thumb_slider_products = $document->find('.products-thumb-slider__slide');
+            $thumb_slider_products = $document->find('.products-thumb-slider__slide');
 
-        $items = [];
+            $items = [];
 
-        foreach ($main_slider_products as $element) {
-            $items['main_slider'][] = [
-                'img' => $element->find('img')[0]->getAttribute('src'),
-                'name' => $element->find('.title')[0]->text(),
-                'weight' => $element->find('.js-weight')[0]->text(),
-                'new_price' => $this->priceConvertor($element->find('.js-current')[0]->html()),
-                'old_price' => $this->priceConvertor($element->find('.js-old')[0]->html()),
-            ];
+            foreach ($main_slider_products as $element) {
+                $items['main_slider'][] = [
+                    'img' => $element->find('img')[0]->getAttribute('src'),
+                    'name' => $element->find('.title')[0]->text(),
+                    'weight' => $element->find('.js-weight')[0]->text(),
+                    'new_price' => $this->priceConvertor($element->find('.js-current')[0]->html()),
+                    'old_price' => $this->priceConvertor($element->find('.js-old')[0]->html()),
+                ];
+            }
+
+            foreach ($thumb_slider_products as $element) {
+                $items['thumb_slider'][] = [
+                    'img' => $element->find('img')[0]->getAttribute('src'),
+                    'name' => $element->find('.title')[0]->text(),
+                    'weight' => $element->find('.js-weight')[0]->text(),
+                    'new_price' => $this->priceConvertor($element->find('.js-current')[0]->html()),
+                    'old_price' => $this->priceConvertor($element->find('.js-old')[0]->html()),
+                ];
+            }
+
+            $json = json_encode($items, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+            $name = 'discount-' . date("d.m.Y") . '.json';
+
+            $file_path = 'public/lotok/' . $name;
+            if (!is_dir(dirname($file_path))) {
+                mkdir(dirname($file_path), 0755, true);
+            }
+
+            file_put_contents($file_path, $json);
+
+            $this->info('File created successfully! Link to it - ' . env('APP_URL') . '/lotok/' . $name);
+        } catch (\Exception $e) {
+            $this->error('Error ' . $e);
         }
-
-        foreach ($thumb_slider_products as $element) {
-            $items['thumb_slider'][] = [
-                'img' => $element->find('img')[0]->getAttribute('src'),
-                'name' => $element->find('.title')[0]->text(),
-                'weight' => $element->find('.js-weight')[0]->text(),
-                'new_price' => $this->priceConvertor($element->find('.js-current')[0]->html()),
-                'old_price' => $this->priceConvertor($element->find('.js-old')[0]->html()),
-            ];
-        }
-
-        $json = json_encode($items, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-
-        $name = 'discount-' . date("d.m.Y") . '.json';
-
-        $file_path = 'public/lotok/' . $name;
-        if (!is_dir(dirname($file_path))) {
-            mkdir(dirname($file_path), 0755, true);
-        }
-
-        file_put_contents($file_path, $json);
-
-        $this->info('File created successfully! Link to it - ' . env('APP_URL') . '/lotok/' . $name);
     }
 }
